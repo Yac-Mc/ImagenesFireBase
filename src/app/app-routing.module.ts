01f95ac6d8b08ebaf23844d982cb178f4c7332ec
Yoe Cardenas <yoe.cardenas@cun.edu.co>
2020-09-12 11:32:23 -0500

    Se agrega estructura de componentes, modelo y servicio

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { FotosComponent } from './component/fotos/fotos.component';
+import { CargaComponent } from './component/carga/carga.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'fotos', component: FotosComponent },
+  { path: 'carga', component: CargaComponent },
+  { path: '**', pathMatch: 'full', redirectTo: 'fotos' }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
